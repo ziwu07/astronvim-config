40122d51e55125d527c80ef7cda06c4635c326c0
ziwu07 <76572511+ziwu07@users.noreply.github.com>
2024-09-17 13:01:38 -0400

    2024-09-17

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -30,10 +30,10 @@ return {
         spell = false, -- sets vim.opt.spell
         signcolumn = "yes", -- sets vim.opt.signcolumn to yes
         wrap = false, -- sets vim.opt.wrap
-        shiftwidth = 4,
-        softtabstop = 4,
-        expandtab = true,
-        tabstop = 4,
+        -- shiftwidth = 4,
+        -- softtabstop = 4,
+        -- expandtab = true,
+        -- tabstop = 4,
       },
       g = { -- vim.g.<key>
         -- configure global vim variables (vim.g)
